58ef495537b7478739fd35a65835a9d9db3e273b
Tevessan <st10091248@vcconnect.edu.za>
2024-11-28 00:03:48 +0200

    edited vite

--- vite.config.js
+++ vite.config.js
@@ -7,51 +7,68 @@ import path from 'path';
 import child_process from 'child_process';
 import { env } from 'process';
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+export default defineConfig(({ mode }) => {
+    let serverConfig = {};
 
-const certificateName = "vcshuttleservice.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+    if (mode !== 'production') {
+        const baseFolder =
+            env.APPDATA !== undefined && env.APPDATA !== ''
+                ? `${env.APPDATA}/ASP.NET/https`
+                : `${env.HOME}/.aspnet/https`;
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
+        const certificateName = "vcshuttleservice.client";
+        const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+        const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7161';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [plugin()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        proxy: {
-            '^/weatherforecast': {
-                target,
-                secure: false
+        if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+            if (
+                0 !==
+                child_process.spawnSync(
+                    'dotnet',
+                    [
+                        'dev-certs',
+                        'https',
+                        '--export-path',
+                        certFilePath,
+                        '--format',
+                        'Pem',
+                        '--no-password',
+                    ],
+                    { stdio: 'inherit' }
+                ).status
+            ) {
+                throw new Error("Could not create certificate.");
             }
-        },
-        port: 5173,
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
         }
+
+        const target = env.ASPNETCORE_HTTPS_PORT
+            ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+            : env.ASPNETCORE_URLS
+            ? env.ASPNETCORE_URLS.split(';')[0]
+            : 'https://localhost:7161';
+
+        serverConfig = {
+            proxy: {
+                '^/weatherforecast': {
+                    target,
+                    secure: false,
+                },
+            },
+            port: 5173,
+            https: {
+                key: fs.readFileSync(keyFilePath),
+                cert: fs.readFileSync(certFilePath),
+            },
+        };
     }
-})
+
+    return {
+        plugins: [plugin()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+        server: serverConfig,
+    };
+});
